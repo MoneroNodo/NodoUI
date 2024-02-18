be6b90797ee22ad4fa481769f58fd1372d8d62b2
intr-cx <15019654+intr-cx@users.noreply.github.com>
2024-02-18 14:20:14 +0100

    Hide mouse pointer

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,33 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <Qt>
 
 #include "NodoConfigParser.h"
 #include "NodoFeedParser.h"
 #include "NodoSystemControl.h"
 
 int main(int argc, char *argv[]) {
-    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+	qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
-    QApplication app(argc, argv);
-    QQmlApplicationEngine engine;
-    //    app.setOverrideCursor(Qt::BlankCursor);
+	QApplication app(argc, argv);
+	QQmlApplicationEngine engine;
+	app.setOverrideCursor(Qt::BlankCursor);
+
+	NodoConfigParser *configParser = new NodoConfigParser();
+	NodoSystemControl *systemControl = new NodoSystemControl(configParser);
+	NodoFeedParser *feedParser = new NodoFeedParser(configParser);
 
 
-    NodoConfigParser *configParser = new NodoConfigParser();
-    NodoSystemControl *systemControl = new NodoSystemControl(configParser);
-    NodoFeedParser *feedParser = new NodoFeedParser(configParser);
+	engine.rootContext()->setContextProperty("nodoControl", systemControl);
+	engine.rootContext()->setContextProperty("feedParser", feedParser);
 
+	engine.addImportPath( ":/" );
+	engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
-    engine.rootContext()->setContextProperty("nodoControl", systemControl);
-    engine.rootContext()->setContextProperty("feedParser", feedParser);
-
-    engine.addImportPath( ":/" );
-    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
-
-    if (engine.rootObjects().isEmpty())
-        return -1;
-    return app.exec();
+	if (engine.rootObjects().isEmpty())
+		return -1;
+	return app.exec();
 }
 
 
