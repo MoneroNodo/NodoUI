4c359982992f5b4695c7fea90cca17a532cad9d5
Cihan <you@example.com>
2024-09-08 15:35:29 +0100

    commented out test code

--- NodoUI/src/NodoSystemControl.cpp
+++ NodoUI/src/NodoSystemControl.cpp
@@ -309,8 +309,8 @@ QString NodoSystemControl::getGPUUsage(void)
 void NodoSystemControl::setPassword(QString pw)
 {
     enableComponent(false);
-    QTimer::singleShot(4000, this, SLOT(testSlotFunction()));
-    // m_dbusController->setPassword(pw);
+    // QTimer::singleShot(4000, this, SLOT(testSlotFunction()));
+    m_dbusController->setPassword(pw);
 }
 
 void NodoSystemControl::serviceManager(QString operation, QString service)
